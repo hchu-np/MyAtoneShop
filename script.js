17ae402856c17a6469edcba38ef9550cf1d967e9
h.chu <hchu@netprotections.co.jp>
2025-02-20 10:52:03 +0700

    update banner

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function greetUser() {
-    var name = document.getElementById("name").value;
-    var greetingMessage = document.getElementById("greetingMessage");
-    if (name) {
-        greetingMessage.textContent = "Chào " + name + "!";
-    } else {
-        greetingMessage.textContent = "Vui lòng nhập tên của bạn.";
-    }
-}
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
