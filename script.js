da4e138d5217fdb2059105286e7c3d2ad426f3fa
h.chu <hchu@netprotections.co.jp>
2025-02-25 10:36:35 +0700

    handle click action on navigation button

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+const nextButton = document.querySelector('.swiper-button-next');
+const prevButton = document.querySelector('.swiper-button-prev');
+let currentIndex = 0;
+const slides = document.querySelectorAll('.my-swiper-slide');
+
+hideAllSlides();
+showCurrentSlide();
+
+nextButton.addEventListener('click', function () {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+  showCurrentSlide();
+});
+
+prevButton.addEventListener('click', function () {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
+  }
+  showCurrentSlide();
+
+});
+
+
+function showCurrentSlide() {
+  hideAllSlides();
+  slides[currentIndex].style.display = 'grid';
+}
+
+function hideAllSlides() {
+  slides.forEach(slide => {
+    slide.style.display = 'none';
   });
+}
